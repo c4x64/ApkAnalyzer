4dd1b9f27988d45772ab403da9d1db784c7ea54f
c4x64 <bloodmeridian.prabhas@gmail.com>
2026-04-22 12:35:49 +0545

    Fix Windows compatibility for MemoryScanner

--- src/MemoryScanner.cpp
+++ src/MemoryScanner.cpp
@@ -1,12 +1,20 @@
 #include "MemoryScanner.hpp"
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <unistd.h>
 #include <sys/mman.h>
+#endif
 
 bool MemoryScanner::isReadable(uintptr_t address) {
     if (address == 0) return false;
-    // Check if memory is accessible using mincore or msync
+#ifdef _WIN32
+    MEMORY_BASIC_INFORMATION mbi;
+    return VirtualQuery((LPCVOID)address, &mbi, sizeof(mbi)) && (mbi.Protect & PAGE_READONLY || mbi.Protect & PAGE_READWRITE || mbi.Protect & PAGE_EXECUTE_READ);
+#else
     unsigned char vec;
     return mincore((void*)address, 1, &vec) != -1;
+#endif
 }
 
 uintptr_t MemoryScanner::findSectionBase(const std::string& sectionName) {
