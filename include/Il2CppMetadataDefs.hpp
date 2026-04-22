2f2ce3c4b3aa1640bbb12dddea7b54ad7d71de4b
c4x64 <bloodmeridian.prabhas@gmail.com>
2026-04-22 12:13:52 +0545

    Add Il2CppMethodDefinition structure for metadata parsing

---
+++ include/Il2CppMetadataDefs.hpp
@@ -0,0 +1,12 @@
+struct Il2CppMethodDefinition {
+    int32_t nameIndex;
+    int32_t declaringType;
+    int32_t returnType;
+    int32_t parameterStart;
+    int32_t customAttributeIndex;
+    int32_t token;
+    uint16_t flags;
+    uint16_t iflags;
+    uint16_t slot;
+    uint16_t parameterCount;
+};
