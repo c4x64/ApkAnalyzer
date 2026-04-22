62b305b4aebd86f6812569664ee2fb0a804b57c9
c4x64 <bloodmeridian.prabhas@gmail.com>
2026-04-22 12:15:20 +0545

    Add Il2CppCodeRegistration structure

---
+++ include/Il2CppCodeRegistration.hpp
@@ -0,0 +1,9 @@
+struct Il2CppCodeRegistration {
+    uint32_t methodPointersCount;
+    uint32_t methodPointers;
+    uint32_t adjustorThunks;
+    uint32_t invokerPointers;
+    uint32_t reverseLimiters;
+    uint32_t genericMethodPointers;
+    uint32_t invokerIndices;
+};
