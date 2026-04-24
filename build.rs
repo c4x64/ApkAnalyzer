a8d3b1731a0610448f9ffce3c4d1425ab52e3a4a
c4x64 <bloodmeridian.prabhas@gmail.com>
2026-04-24 10:33:57 +0545

    build: update build.rs to include native DEX parser

--- build.rs
+++ build.rs
@@ -2,7 +2,11 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .file("native/src/utils/math_utils.cpp")
+        .file("native/src/parsers/dex_parser.cpp")
+        .include("native/include")
         .compile("native_core");
 
     println!("cargo:rerun-if-changed=native/src/utils/math_utils.cpp");
+    println!("cargo:rerun-if-changed=native/src/parsers/dex_parser.cpp");
+    println!("cargo:rerun-if-changed=native/include/dex_header.hpp");
 }
